fb9e38d2c02e2c872473b593e9eadc7f4a0d5c9d
Jake Chavez <jake94chavez@gmail.com>
2018-02-07 13:51:32 -0600

    axios setup complete (still needs testing)

--- src/models/Post.js
+++ src/models/Post.js
@@ -12,6 +12,20 @@ class PostModel {
 	static getOne(postId) {
 		let url = 'https://roughdraftapi.herokuapp.com/api/posts' + postId;
 		let request = axios.get(url);
-		return request
+		return request;
+	}
+	static delete(post) {
+		let url = 'https://roughdraftapi.herokuapp.com/api/posts' + post._id
+		let request = axios.delete(url);
+		return request;
+	}
+	static update(postId, postBody) {
+		let url = 'https://roughdraftapi.herokuapp.com/api/posts' + postId;
+		let request = axios.put(url, {
+			body: postBody
+		});
+		return request;
 	}
 }
+
+export default PostModel;
